daf57e6840600618c89d8eba2764bb4b4980e06d
Anastasios Mitsigkolas <tasos1109@gmail.com>
2025-07-20 17:59:24 +0200

    Make matrix algorithms more generic for different types of matrices

--- core/include/matrix_algorithms.h
+++ core/include/matrix_algorithms.h
@@ -1,14 +1,12 @@
 #ifndef MATRIX_ALGORITHMS_H
 #define MATRIX_ALGORITHMS_H
 
-// Forward declaration of Matrix class
-template <typename T, my_size_t Rows, my_size_t Cols>
-class Matrix;
+#include "../../tests/utilities.h"
 
 namespace matrix_algorithms
 {
-    template <typename T, my_size_t Rows, my_size_t Cols>
-    Matrix<T, Rows, Cols> choleskyDecomposition(Matrix<T, Rows, Cols> &matrix) // TODO: the matrix should be const
+    template <typename MatrixType>
+    MatrixType choleskyDecomposition(const MatrixType &matrix)
     {
         // Check if the matrix is symmetric, checking if it's square has been done in the isSymmetric function
         if (!matrix.isSymmetric())
@@ -17,14 +15,14 @@ namespace matrix_algorithms
         }
 
         // Create a new zero-initialized matrix to store the result
-        Matrix<T, Rows, Cols> result(0);
+        MatrixType result(0);
 
         // Perform the Cholesky decomposition
         for (my_size_t i = 0; i < matrix.getDim(0); ++i)
         {
             for (my_size_t j = 0; j <= i; ++j)
             {
-                T sum = 0;
+                typename MatrixType::value_type sum = 0;
                 for (my_size_t k = 0; k < j; ++k)
                 {
                     sum += result(i, k) * result(j, k);
@@ -32,8 +30,8 @@ namespace matrix_algorithms
 
                 if (i == j) // Diagonal element
                 {
-                    T diag = matrix(i, i) - sum;
-                    if (diag <= T(PRECISION_TOLERANCE))
+                    typename MatrixType::value_type diag = matrix(i, i) - sum;
+                    if (diag <= typename MatrixType::value_type(PRECISION_TOLERANCE))
                     {
                         throw std::runtime_error("Matrix is not positive definite");
                     }
@@ -41,7 +39,7 @@ namespace matrix_algorithms
                 }
                 else // Off-diagonal element
                 {
-                    result(i, j) = (matrix(i, j) - sum) / result(j, j);
+                    result(i, j) = (matrix(i, j) - sum) / result(j, j); //<-- this line
                 }
             }
         }
