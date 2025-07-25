dceb915748421e68dcbb9d0d1c4aa421d4058f23
Anastasios Mitsigkolas <tasos1109@gmail.com>
2025-07-25 10:26:40 +0200

    FusedMatrix & Matrix benchmarks polishing

--- tests/test_compare_matrix_and_fused_matrix.cpp
+++ tests/test_compare_matrix_and_fused_matrix.cpp
@@ -3,26 +3,26 @@
 #include "matrix.h"
 #include "utilities.h"
 
-TEST_CASE("FusedMatrix & Matrix benchmarks", "[fused_matrix, matrix]")
+TEST_CASE("FusedMatrix & Matrix benchmarks", "[fused_benchmark]")
 {
-    FusedMatrix<double, 100, 100> fmat1(1), fmat2(2), fmat3(3), fmat4(10);
-    Matrix<double, 100, 100> mat1(1), mat2(2), mat3(3), mat4(10);
+    FusedMatrix<double, 100, 100> fmat1(1), fmat2(2), fmat3(3), fmat4(10), fmat5(10);
+    Matrix<double, 100, 100> mat1(1), mat2(2), mat3(3), mat4(10), mat5(10);
 
     SECTION("Long operations benchmark")
     {
         std::cout << "Benchmarking long operations on FusedMatrix and Matrix" << std::endl;
         tick();
-        for (int i = 0; i < 1000; ++i)
+        for (int i = 0; i < 10000; ++i)
         {
-            fmat4 = fmat1 + fmat2 * fmat3 - fmat1 / fmat2 + fmat3 * fmat4 + fmat1 * fmat2 + fmat3 / fmat4 + fmat1 - fmat2 + fmat3 * fmat4 + fmat1 / fmat2;
+            fmat5 = fmat1 + fmat2 * fmat3 - fmat1 / fmat2 + fmat3 * fmat4 + fmat1 * fmat2 + fmat3 / fmat4 + fmat1 - fmat2 + fmat3 * fmat4 + fmat1 / fmat2;
         }
 
         uint FusedMatrix_time = tock("FusedMatrix long operations");
 
         tick();
-        for (int i = 0; i < 1000; ++i)
+        for (int i = 0; i < 10000; ++i)
         {
-            mat4 = mat1 + mat2 * mat3 - mat1 / mat2 + mat3 * mat4 + mat1 * mat2 + mat3 / mat4 + mat1 - mat2 + mat3 * mat4 + mat1 / mat2;
+            mat5 = mat1 + mat2 * mat3 - mat1 / mat2 + mat3 * mat4 + mat1 * mat2 + mat3 / mat4 + mat1 - mat2 + mat3 * mat4 + mat1 / mat2;
         }
         uint Matrix_time = tock("Matrix long operations");
 
