97ff2d0c2b16c6412c2058bf988c0fe7f970e0fa
Anastasios Mitsigkolas <tasos1109@gmail.com>
2025-08-23 00:40:10 +0200

    FusedTensor & Eigen benchmarks
    Better output messages and titles

--- tests/test_compare_fused_matrix_and_eigen.cpp
+++ tests/test_compare_fused_matrix_and_eigen.cpp
@@ -1,17 +1,18 @@
 #include <catch_amalgamated.hpp>
-#include "fused/fused_matrix.h"
+#include "fused/fused_vector.h"
 #include <Dense>
 #include "utilities.h"
 
 #define EIGEN_NO_MALLOC
 
-TEST_CASE("FusedMatrix & Matrix benchmarks", "[fused_eigen_benchmark]")
+TEST_CASE("FusedTensor & Eigen benchmarks", "[fused_eigen_benchmark]")
 {
-    FusedTensorND<float, 100, 100> fmat1(7.0), fmat2(7.0), fmat3(7.0), fmat4(7.0), fmat5(7.0);
-    Eigen::Matrix<float, 100, 100> mat1, mat2, mat3, mat4, mat5;
+    FusedTensorND<double, 100, 100> fmat1(7.0), fmat2(7.0), fmat3(7.0), fmat4(7.0), fmat5(7.0); // It can work with FusedMatrix amd FusedVector too
+    Eigen::Matrix<double, 100, 100> mat1, mat2, mat3, mat4, mat5;
 
     SECTION("Long operations benchmark")
     {
+        OpTraits<double, BITS, DefaultArch>::test();
 
         mat1.setRandom();
         mat2.setRandom();
@@ -34,12 +35,12 @@ TEST_CASE("FusedMatrix & Matrix benchmarks", "[fused_eigen_benchmark]")
         {
             mat5 = mat1 + mat2 + mat3 - mat1 - mat2 + mat3 + mat4 + mat1 - mat2 + mat3 - mat4 + mat1 - mat2 + mat3 + mat4 + mat1 - mat2;
         }
-        uint Matrix_time = tock("Matrix long operations");
+        uint Matrix_time = tock("Eigen long operations");
 
         // Percentage Increase
         double percentage_increase = (((double)FusedMatrix_time - (double)Matrix_time) / (double)Matrix_time) * 100.0;
 
-        std::cout << "Percentage decrease in time for FusedMatrix compared to Matrix: "
+        std::cout << "Percentage decrease in time for FusedMatrix compared to Eigen: "
                   << percentage_increase << "%" << std::endl;
 
         // // fmat4.print();
