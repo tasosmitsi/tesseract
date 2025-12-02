540dee28bfa0119d1421a1f99464fd799be2bb44
Anastasios <tasos1109@gmail.com>
2025-12-02 20:02:50 +0000

    move inits outside of the benchmarks

--- tests/benchmarks.cpp
+++ tests/benchmarks.cpp
@@ -9,14 +9,59 @@ TEMPLATE_TEST_CASE("Benchmarks", "[benchmarks]", double, float)
 {
     using T = TestType;
 
-    FusedTensorND<T, 100, 100> fmat1, fmat2, fmat3, fmat4, fmat5; // It can work with FusedMatrix amd FusedVector too
+    FusedTensorND<T, 100, 100> fmat1, fmat2, fmat3, fmat4, fmat5, res; // It can work with FusedMatrix amd FusedVector too
+    FusedMatrix<T, 100, 100> matrix1, matrix2;
     Eigen::Matrix<T, 100, 100> mat1, mat2, mat3, mat4, mat5;
+
+    T init_inverse_values[4][4] = {
+        {2.0, -1, 2.0, -1},
+        {4, 5.0, 2.5, -17},
+        {2.0, -1, 2.43, -30},
+        {4, 5.0, 245, -10}};
+
+    T init_cholesky_values[3][3] = {
+        {4, 12, -16},
+        {12, 37, -43},
+        {-16, -43, 98}};
+
+    // Init FusedMatrix
+    FusedMatrix<T, 4, 4> inv_matrix = init_inverse_values;
+    FusedMatrix<T, 4, 4> inv_res;
+
+    FusedMatrix<T, 3, 3> pre_cholesky = init_cholesky_values;
+    FusedMatrix<T, 3, 3> cholesky;
+    // --------------------------
+
+    // Init Eigen Matrix
+    Eigen::Matrix<T, 4, 4> inv_eigen_matrix;
+    Eigen::Matrix<T, 4, 4> inv_eigen_res;
+    for (size_t i = 0; i < 4; ++i)
+    {
+        for (size_t j = 0; j < 4; ++j)
+        {
+            inv_eigen_matrix(i, j) = init_inverse_values[i][j];
+        }
+    }
+
+    Eigen::Matrix<T, 3, 3> pre_eigen_cholesky;
+    for (size_t i = 0; i < 3; ++i)
+    {
+        for (size_t j = 0; j < 3; ++j)
+        {
+            pre_eigen_cholesky(i, j) = init_cholesky_values[i][j];
+        }
+    }
+    Eigen::Matrix<T, 3, 3> cholesky_eigen;
+    // --------------------------
 
     mat1.setRandom();
     mat2.setRandom();
     mat3.setRandom();
     mat4.setRandom();
     mat5.setRandom();
+
+    matrix1.setRandom(10, -10);
+    matrix2.setRandom(10, -10);
 
     fmat1.setRandom(10, -10);
     fmat2.setRandom(10, -10);
@@ -44,121 +89,50 @@ TEMPLATE_TEST_CASE("Benchmarks", "[benchmarks]", double, float)
 
     BENCHMARK("FusedMatrix matmul")
     {
-        FusedMatrix<T, 2, 3> matrix1(2);
-        FusedMatrix<T, 3, 2> matrix2(2);
-        FusedMatrix<T, 2, 2> res;
-
-        matrix1.setHomogen(10);
-        matrix2.setHomogen(33);
-
-        for (int i = 0; i < 100; ++i)
-        {
-            res = FusedMatrix<T, 2, 2>::matmul(matrix1, matrix2);
-        }
+        res = FusedMatrix<T, 100, 100>::matmul(matrix1, matrix2);
         return res;
     };
 
     BENCHMARK("Eigen matmul")
     {
-        Eigen::Matrix<T, 2, 3> matrix1;
-        Eigen::Matrix<T, 3, 2> matrix2;
-        Eigen::Matrix<T, 2, 2> res;
-
-        matrix1.setConstant(10);
-        matrix2.setConstant(33);
-
-        for (int i = 0; i < 100; ++i)
-        {
-            res = matrix1 * matrix2;
-        }
-        return res;
+        mat3 = mat1 * mat2;
+        return mat3;
     };
 
     BENCHMARK("FusedMatrix inverse")
     {
-        // init the matrix
-        T initValues[4][4] = {
-            {2.0, -1, 2.0, -1},
-            {4, 5.0, 2.5, -17},
-            {2.0, -1, 2.43, -30},
-            {4, 5.0, 245, -10}};
-        FusedMatrix<T, 4, 4> matrix3 = initValues;
-        FusedMatrix<T, 4, 4> inv;
-
         for (int i = 0; i < 100; ++i)
         {
-            inv = matrix3.inverse();
+            inv_res = inv_matrix.inverse();
         }
-        return inv;
+        return inv_res;
     };
 
     BENCHMARK("Eigen inverse")
     {
-        Eigen::Matrix<T, 4, 4> matrix3;
-        Eigen::Matrix<T, 4, 4> inv;
-
-        // init the matrix
-        T initValues[4][4] = {
-            {2.0, -1, 2.0, -1},
-            {4, 5.0, 2.5, -17},
-            {2.0, -1, 2.43, -30},
-            {4, 5.0, 245, -10}};
-        for (size_t i = 0; i < 4; ++i)
-        {
-            for (size_t j = 0; j < 4; ++j)
-            {
-                matrix3(i, j) = initValues[i][j];
-            }
-        }
-
         for (int i = 0; i < 100; ++i)
         {
-            inv = matrix3.inverse();
+            inv_eigen_res = inv_eigen_matrix.inverse();
         }
-        return inv;
+        return inv_eigen_res;
     };
 
     BENCHMARK("FusedMatrix Cholesky Decomposition")
     {
-        // init the matrix
-        T initValues[3][3] = {
-            {4, 12, -16},
-            {12, 37, -43},
-            {-16, -43, 98}};
-        FusedMatrix<T, 3, 3> matrix3 = initValues;
-        FusedMatrix<T, 3, 3> cholesky;
-
-        // using tessaract
         for (int i = 0; i < 100; ++i)
         {
-            cholesky = matrix_algorithms::choleskyDecomposition(matrix3);
+            cholesky = matrix_algorithms::choleskyDecomposition(pre_cholesky);
         }
         return cholesky;
     };
 
     BENCHMARK("Eigen Cholesky Decomposition")
     {
-        // init the matrix
-        T initValues[3][3] = {
-            {4, 12, -16},
-            {12, 37, -43},
-            {-16, -43, 98}};
-        Eigen::Matrix<T, 3, 3> matrix3;
-        for (size_t i = 0; i < 3; ++i)
-        {
-            for (size_t j = 0; j < 3; ++j)
-            {
-                matrix3(i, j) = initValues[i][j];
-            }
-        }
-        Eigen::Matrix<T, 3, 3> cholesky;
-
         for (int i = 0; i < 100; ++i)
         {
-            Eigen::LLT<Eigen::Matrix<T, 3, 3>> lltOfA(matrix3);
-            cholesky = lltOfA.matrixL();
+            Eigen::LLT<Eigen::Matrix<T, 3, 3>> lltOfA(pre_eigen_cholesky);
+            cholesky_eigen = lltOfA.matrixL();
         }
-
-        return cholesky;
+        return cholesky_eigen;
     };
 }
