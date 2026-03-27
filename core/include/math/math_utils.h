a5082c8807463c520dc0b558deda91030312c6c9
Anastasios <tasos1109@gmail.com>
2026-03-27 14:48:44 +0000

    add sin, cos, acos to the math utils

--- core/include/math/math_utils.h
+++ core/include/math/math_utils.h
@@ -59,6 +59,48 @@ namespace math
             return x < T(0) ? -x : x;
     }
 
+    /**
+     * @brief Compute the sine of a floating-point value (radians).
+     *
+     * @tparam T Floating-point type (float or double).
+     * @param x Angle in radians.
+     * @return Sine of @p x.
+     */
+    template <typename T>
+    constexpr T sin(T x) noexcept
+    {
+        static_assert(is_floating_point_v<T>, "sin requires a floating-point type");
+        return is_same_v<T, float> ? __builtin_sinf(x) : __builtin_sin(x);
+    }
+
+    /**
+     * @brief Compute the cosine of a floating-point value (radians).
+     *
+     * @tparam T Floating-point type (float or double).
+     * @param x Angle in radians.
+     * @return Cosine of @p x.
+     */
+    template <typename T>
+    constexpr T cos(T x) noexcept
+    {
+        static_assert(is_floating_point_v<T>, "cos requires a floating-point type");
+        return is_same_v<T, float> ? __builtin_cosf(x) : __builtin_cos(x);
+    }
+
+    /**
+     * @brief Compute the arc cosine of a floating-point value.
+     *
+     * @tparam T Floating-point type (float or double).
+     * @param x Value in [-1, 1].
+     * @return Arc cosine in radians [0, π].
+     */
+    template <typename T>
+    constexpr T acos(T x) noexcept
+    {
+        static_assert(is_floating_point_v<T>, "acos requires a floating-point type");
+        return is_same_v<T, float> ? __builtin_acosf(x) : __builtin_acos(x);
+    }
+
 } // namespace math
 
 #endif // MATH_UTILS_H
