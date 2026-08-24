004eeaf15f528d50cc2f451939631fbcca8eb6cc
Anastasios <tasos1109@gmail.com>
2026-08-24 10:12:46 +0000

    doxy documentation

--- core/include/fused/operators/reductions.h
+++ core/include/fused/operators/reductions.h
@@ -4,10 +4,15 @@
 #include "fused/kernel_ops/kernel_ops.h"
 #include "algebra/algebraic_traits.h"
 
-// ===============================
-// Min/Max/Sum Reduction Functions
-// ===============================
+/**
+ * @file reductions.h
+ * @brief Whole-tensor reductions to a scalar.
+ *
+ * Not to be confused with the element-wise min/max in operators/min_max.h:
+ * min(expr) returns a scalar, min(expr, expr) returns an expression.
+ */
 
+/// @brief Smallest element of the tensor.
 template <typename Expr>
     requires(algebra::is_tensor_v<Expr> &&
              !algebra::is_algebra_v<Expr>)
@@ -16,6 +21,7 @@ typename Expr::value_type min(const BaseExpr<Expr> &expr)
     return KernelOps<typename Expr::value_type, BITS, DefaultArch>::reduce_min(expr.derived());
 }
 
+/// @brief Largest element of the tensor.
 template <typename Expr>
     requires(algebra::is_tensor_v<Expr> &&
              !algebra::is_algebra_v<Expr>)
@@ -24,6 +30,7 @@ typename Expr::value_type max(const BaseExpr<Expr> &expr)
     return KernelOps<typename Expr::value_type, BITS, DefaultArch>::reduce_max(expr.derived());
 }
 
+/// @brief Sum of every element of the tensor.
 template <typename Expr>
     requires(algebra::is_tensor_v<Expr> &&
              !algebra::is_algebra_v<Expr>)
